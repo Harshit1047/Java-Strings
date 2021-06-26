03c79e739940b6ebad597086a900c8c01d7d43d4
Harshit Pratap Singh <86252946+Harshit1047@users.noreply.github.com>
2021-06-26 17:15:37 +0530

    Add files via upload

---
+++ equals.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+class eq{
+    public static void main(String[] args)
+    {
+        String str="Java is a program";
+        String str1="Java is a program";
+        boolean str2=str.equals(str1);
+        System.out.println(str2);
+    }
+
+}
---
+++ equalsIgnoreCase.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+class ig{
+    public static void main(String[] args)
+    {
+        String str="java is a program";
+        String str1="Java is a program";
+        boolean str2=str.equalsIgnoreCase(str1);
+        System.out.println(str2);
+    }
+
+}
