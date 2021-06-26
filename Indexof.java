eacc2a6aca2af96b60e286c2a7fea210bdf3217b
Harshit Pratap Singh <86252946+Harshit1047@users.noreply.github.com>
2021-06-26 17:08:45 +0530

    Add files via upload

---
+++ Indexof.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+class in{
+    public static void main(String[] args)
+    {
+        String str="Java is a program";
+        int str1=str.indexOf("Java");
+        System.out.println(str1);
+    }
+}
---
+++ LastIndexOf.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+class la{
+    public static void main(String[] args)
+    {
+        String str="Java is a program";
+        int str1=str.lastIndexOf("J");
+        System.out.println(str1);
+    }
+
+}
