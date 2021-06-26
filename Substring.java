ec676f5e62285a201a3f7ec9d23d3c70b7a2d17a
Harshit Pratap Singh <86252946+Harshit1047@users.noreply.github.com>
2021-06-26 11:38:51 +0530

    Add files via upload

---
+++ Substring.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+class sub{
+    public static void main(String[] args)
+    {
+String str="Java is a program";
+String str2=str.substring(8);
+System.out.println(str2);
+    }
+}
