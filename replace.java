95da1e3827a403618ab305826be7b74f85b1d644
Harshit Pratap Singh <86252946+Harshit1047@users.noreply.github.com>
2021-06-26 12:46:54 +0530

    Add files via upload

---
+++ replace.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+class repla{
+    public static void main(String[] args)
+    {
+        String str="Java is a programming Language";
+        String str1=str.replace('J','k');
+        System.out.println(str1);
+    }
+}
