47a9dad499f8cea618e8fa0735b81a77246bfd03
Harshit Pratap Singh <86252946+Harshit1047@users.noreply.github.com>
2021-06-26 12:47:13 +0530

    Add files via upload

---
+++ endsWith.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+class start{
+    public static void main(String[] args)
+    {
+        String str="Java is a programming Language";
+        boolean str1=str.endsWith("Language");
+        System.out.println(str1);
+    }
+}
