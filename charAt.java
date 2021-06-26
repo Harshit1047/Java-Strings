4fd9b47fdb2f607cdf8d78825633d1011624aaaf
Harshit Pratap Singh <86252946+Harshit1047@users.noreply.github.com>
2021-06-26 17:03:06 +0530

    Add files via upload

---
+++ charAt.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+class ch{
+    public static void main(String[] args)
+    {
+        String str="Java is a program";
+        char str1=str.charAt(8);
+        System.out.println(str1);
+    }
+}
