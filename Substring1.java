589f6cb39f87a7cae48be8b4b6563281dc7761de
Harshit Pratap Singh <86252946+Harshit1047@users.noreply.github.com>
2021-06-26 12:15:06 +0530

    Add files via upload

---
+++ Substring1.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+class subs{
+    public static void main(String[] args)
+    {
+        String str="Java is a programming Language";
+        String str1=str.substring(1,3);
+        System.out.println(str1);
+    }
+}
