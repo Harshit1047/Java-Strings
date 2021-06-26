6208e76f71c9f40cda27521730a37b3d0efc0cb4
Harshit Pratap Singh <86252946+Harshit1047@users.noreply.github.com>
2021-06-26 12:32:26 +0530

    Add files via upload

---
+++ StartsWith.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+class start{
+    public static void main(String[] args)
+    {
+        String str="Java is a programming Language";
+        boolean str1=str.startsWith("Java");
+        System.out.println(str1);
+    }
+}
